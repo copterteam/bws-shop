210c337c5019fcd92ad0550cc92059e54f90e061
copterteam <rucopterteam@gmail.com>
2019-12-14 17:04:29 +0300

    shipping

---
+++ shipping.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en" class="wide wow-animation">
+  <head>
+    <!-- Site Title-->
+    <title>Оплата и доставка товара | БВС Системы</title>
+    <meta name="format-detection" content="telephone=no">
+    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <meta charset="utf-8">
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
+    <!-- Stylesheets-->
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Roboto:100,300,400,500">
+    <link rel="stylesheet" href="css/style.css">
+		<!--[if lt IE 10]>
+    <div style="background: #212121; padding: 10px 0; box-shadow: 3px 3px 5px 0 rgba(0,0,0,.3); clear: both; text-align:center; position: relative; z-index:1;"><a href="http://windows.microsoft.com/en-US/internet-explorer/"><img src="images/ie8-panel/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today."></a></div>
+    <script src="js/html5shiv.min.js"></script>
+		<![endif]-->
+  </head>
+  <body>
+    <!-- Page-->
+    <div class="page text-center">
+   
+   <?include("header.php"); ?>
+  <!-- Page Content-->
+      <main class="page-content">
+        <!-- ABOUT US-->
+       <section class="section-35 section-sm-top-75 section-sm-bottom-0 text-center">
+          <div class="shell">
+            <div class="range range-sm-center">
+              <div class="cell-xs-12 cell-md-10">
+                              <h5>Оплата и доставка</h5>
+              </div>
+			   <p>
+			   Осуществляем продажу продукции как физическим, так и юридическим лицам. Принимаем все виды безналичных платежей. Для физических лиц - оплата картой или электронными деньгами на сайте, для юридических лиц - выставляем счет для оплаты заказа. Для получения счета и заключения договора - отправьте реквизиты компании на электронную почту shop@bws-shop.ru.</p>
+            </div>
+          </div>
+        </section>
+
+        <!-- Our benefits-->
+        <section class="section-bottom-150 text-center">
+          <div class="shell">
+            <div class="range range-sm-center">
+              <div class="cell-xs-12 cell-md-10">
+                <div class="hr-vertical"></div>
+                <h5>Варианты доставки</h5>
+              </div>
+            </div>
+            <div class="range">
+              <div class="cell-xs-12 cell-sm-6 cell-md-4">
+                <h6>Самовывоз со склада</h6>
+                <p>Самостоятельно забрать заказ со вклада в Санкт-Петербурге возможно по будням с 10:00 до 20:00. Самовывоз осуществляется после полной оплаты заказа и получения подтверждения о его готовности к отгрузке. </p>
+              </div>
+              <div class="cell-xs-12 cell-sm-6 cell-md-4 offset-top-45 offset-sm-top-0">
+                <h6>Курьерская служба</h6>
+                <p>Отправка на адрес клиента по Санкт-Петербургу и области осуществляется на следующий рабочий день после полной оплаты заказа. Стоимость курьерской доставки расчитывается исходя из объема и массы заказанных наименований.  </p>
+              </div>
+              <div class="cell-xs-12 cell-sm-6 cell-md-4 offset-top-45 offset-md-top-0">
+                <h6>Транспортная компания</h6>
+                <p>Доставка в регионы осуществляется транспортной компанией "Деловые линии". Перемещение до терминала бесплатно. Стоимость перевозки зависит от массы и объема груза и расчитывается по тарифам транспортной компании.</p>
+              </div>
+            </div>
+			  </div>
+        </section>
+
+
+		  
+      </main>
+	  
+  <?include("footer.php"); ?>
+
+    </div>
+
+    <!-- Java script-->
+    <script src="js/core.min.js"></script>
+	<script src="js/jquery.validate.min.js"></script>
+    <script src="js/script.js"></script>
+  </body>
+</html>
